ce95d73d0f6321ff2a29097a57058f358b270d0f
tHaraldsson <tommy.haraldsson@stud.sti.se>
2025-02-20 14:19:59 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // Byt ut vid deployment
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log("En användare anslöt", socket.id);
+
+  socket.on("message", (data) => {
+    console.log("Meddelande mottaget:", data);
+    io.emit("message", data); // Skickar vidare till alla anslutna klienter
+  });
+
+  socket.on("disconnect", () => {
+    console.log("En användare kopplade från", socket.id);
+  });
+});
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Servern körs på port ${PORT}`);
+});
